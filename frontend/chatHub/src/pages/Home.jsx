a27fbaf79f4e2297678fbef225884607a381e17b
Tripti788 <144474933+Tripti788@users.noreply.github.com>
2025-07-05 20:07:29 +0000

    some changes in app

--- frontend/chatHub/src/pages/Home.jsx
+++ frontend/chatHub/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import bg_image from '../assets/bg_image.png';
+import bg_image from '../assets/bg_image.jpg';
 // import bg_image2 from '../assets/bg_image2.jpg';
 import './Home.css'; // optional if you want custom styling
 import { useNavigate } from 'react-router-dom';
