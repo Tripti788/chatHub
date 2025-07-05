7a5aba0e07d1e79c23e59fc36f79afbc724495c9
Tripti788 <144474933+Tripti788@users.noreply.github.com>
2025-07-05 17:41:58 +0000

    some changes in app

--- frontend/chatHub/src/pages/Home.jsx
+++ frontend/chatHub/src/pages/Home.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 import bg_image from '../assets/bg_image';
 import large_set_of_flat_messages from '../assets/large_set_of_flat_messages.jpg';
+import './Home.css'; // optional if you want custom styling
+
 const Home = () => {
   return (
-    <div className='d-flex'>
-        <div>
-            <img src={bg_image} alt="" />
-
-        </div>
-        <div>
-            <img src={large_set_of_flat_messages} alt="" />
-        </div>
-      
+    <div className="home-container">
+      <div className="image-wrapper">
+        <img src={bg_image} alt="Background Illustration" />
+      </div>
+      <div className="image-wrapper">
+        <img src={large_set_of_flat_messages} alt="Messages Illustration" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
